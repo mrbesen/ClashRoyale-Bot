07c134f7c62d2e5267339271e94739641c61b638
mrbesen <y.g.2@gmx.de>
2017-11-26 19:52:53 +0100

    Size Ajustment for windows, removed Overlay button for next release

--- src/mrbesen/cr/auto/clicker/UI.java
+++ src/mrbesen/cr/auto/clicker/UI.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -94,7 +95,7 @@ public class UI implements ActionListener {
 		
 		//init screen
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		frame.setSize(830, 180);
+		frame.setSize(830,( System.getProperty("os.name").toLowerCase().contains("win") ? 220 : 180));//extra large for windows
 
 		save.setText("Save");
 		save.addActionListener(this);
@@ -131,7 +132,7 @@ public class UI implements ActionListener {
 		middle.add(skip);
 		middle.add(pause);
 		middle.add(exit);
-		middle.add(overlay);
+		//middle.add(overlay);//added later
 		middle.add(autoplay);
 		middle.add(doubleplace);
 		middle.add(backfocus);
